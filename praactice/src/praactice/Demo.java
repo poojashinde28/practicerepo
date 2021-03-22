440195f44813cf99c4f022e836318993469d9384
poojashinde28 <30907692+poojashinde28@users.noreply.github.com>
2021-03-22 21:48:30 +0530

    updated

--- praactice/src/praactice/Demo.java
+++ praactice/src/praactice/Demo.java
@@ -5,6 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi there");
+		int a=10;
+		System.out.println(a);
+		
 
 	}
 
